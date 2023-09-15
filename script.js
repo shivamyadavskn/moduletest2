b4a54f41b7d97c2f65cd72e652fa81876d12d445
Avi Vashishta <avivashishta@gmail.com>
2023-09-15 13:04:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+let arr = [
+  { id: 1, name: 'john', age: 18, profession: 'developer' },
+  { id: 2, name: 'jack', age: 20, profession: 'developer' },
+  { id: 3, name: 'karen', age: 19, profession: 'admin' },
+];
+
+function PrintDeveloper() {
+  //Write your code here , just console.log
+}
+
+function addData() {
+  //Write your code here, just console.log
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+}
+
+function concatenateArray() {
+  //Write your code here, just console.log
+}
+
+function averageAge() {
+  //Write your code here, just console.log
+}
+
+function checkAgeAbove25() {
+  //Write your code here, just console.log
+}
+
+function uniqueProfessions() {
+  //Write your code here, just console.log
+}
+
+function sortByAge() {
+  //Write your code here, just console.log
+}
+
+function updateJohnsProfession() {
+  //Write your code here, just console.log
+}
+
+function getTotalProfessions() {
+  //Write your code here, just console.log
+}
+
+function consoleArr() {
+  console.log('Consoling Array Variable', arr);
+}
